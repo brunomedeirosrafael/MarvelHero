da4cd4a510bdee3b4bb94cb80baa32ed51cfd31a
Bruno Medeiros Rafael <brafael@ciandt.com>
2018-01-07 12:22:59 -0200

    Initial Commit

---
+++ MarvelHero/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarvelHero
+//
+//  Created by Bruno Medeiros Rafael on 07/01/2018.
+//  Copyright © 2018 Bruno Medeiros Rafael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
